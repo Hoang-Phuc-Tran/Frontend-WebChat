a7e347901a0b485007c2155210f58aed489bd5cd
ptran9102 <hoangphuctran9102@gmail.com>
2023-12-28 00:51:10 -0500

    Deploy

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://hoangphucchat.up.railway.app/api";
 export const MEDIA_URL = "https://hoangphucchat.up.railway.app";
-export const WS_ROOT = "wss://hoangphucchat.up.railway.app";
+export const WS_ROOT = "wss://hoangphucchat.up.railway.app/";
